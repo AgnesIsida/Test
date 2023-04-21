251588684d2ae597cab080ebbde46edc0620dfbf
[Agnes] <afisidona@gmail.com>
2023-04-21 22:22:24 +0200

    ok

--- script.js
+++ script.js
@@ -7,4 +7,37 @@ function newFunction() {
     console.log(y);
     const Mail = 0999;
 }
+let name = "Nika"
+let surname = "Ivinski"
+console.log( typeof surname)
+console.log(Math.ceil(99.99))
+function showMessage(message){
+    console.log (messagee)
+}
+showMessage ("Be happy")
+
+function babyTime(play,sleep){
+    return play * sleep
+    }
+console.log babyTime((5,2))
+
+newFunction();
+
+
+
+
+
+   
+        const xreateUser = (name, age, email) => {
+            let user ={
+                user: name,
+                age: age,
+                userEmail: email
+
+            }
+           return user     
+           
+        };
+        console.log(xreateUser("Inna", 33, Reap));
+    
 
