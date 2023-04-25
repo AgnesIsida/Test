14d344167151f979b380b0958438be0dba18414b
[Agnes] <afisidona@gmail.com>
2023-04-25 23:16:33 +0200

    5.5

---
+++ Massive.js
@@ -0,0 +1,14 @@
+
+let value1 = Math.floor(Math.random() * 10);
+  let value2 = 5;
+  let Sum = value1 + value2;
+  console.log(Sum);
+  let result = Sum / 5 + 17;
+  console.log(result)
+  
+
+
+
+
+
+  
